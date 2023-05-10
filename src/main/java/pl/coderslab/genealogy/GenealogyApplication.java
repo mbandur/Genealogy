0e626dfa7943f18ffc91ce82143dbbdaad7278d9
mbandur <mariusz.bandurowicz@gmail.com>
2023-05-10 18:06:36 +0200

    Add initial project structure

---
+++ src/main/java/pl/coderslab/genealogy/GenealogyApplication.java
@@ -0,0 +1,11 @@
+package pl.coderslab.genealogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenealogyApplication.class, args);
+    }
+}
